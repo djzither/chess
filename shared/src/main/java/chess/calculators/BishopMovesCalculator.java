5c88ce1ae9661deb096618220818c45b1d335a30
Derek <dj.zither@gmail.com>
2026-01-20 08:38:07 -0700

    working on BishopMovesCalculator

--- shared/src/main/java/chess/calculators/BishopMovesCalculator.java
+++ shared/src/main/java/chess/calculators/BishopMovesCalculator.java
@@ -4,6 +4,8 @@ import chess.ChessBoard;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,16 +13,39 @@ public class BishopMovesCalculator {
     private final ChessBoard board;
     private final ChessPosition position;
     private final ChessPiece.PieceType pieceType;
-    public BishopMovesCalculator(ChessPiece.PieceType piecetype, ChessPosition position, ChessBoard board, ChessBoard board1, ChessPosition position1, ChessPiece.PieceType pieceType) {
-        this.board = board1;
-        this.position = position1;
+    public BishopMovesCalculator(ChessPosition position, ChessBoard board, ChessPiece.PieceType pieceType) {
+        this.board = board;
+        this.position = position;
         this.pieceType = pieceType;
     }
 
     public List<ChessPosition> getPathValid() {
         int row = position.getRow();
-        int 
+        int col = position.getColumn();
 
+        ChessPiece piece = board.getPiece(position);
+
+        List<int[]> bishop_valid = new ArrayList<>();
+
+        List<int[]> bishop_possible_dir = new ArrayList<>(List.of(new int[]{1, 1}, new int[]{1, -1}, new int[]{-1, 1}, new int[]{-1, -1}));
+
+        for (int[] move : bishop_possible_dir){
+                int x = row;
+                int y = col;
+                for (int i = 1; i <= 8; i++){
+                    x += bishop_possible_dir[0];
+                    y += bishop_possible_dir[1];
+
+                    if (x >= 8 || x <0 ||)
+
+
+
+                }
+
+            }
+            OnBoardAndCapture intance = new OnBoardAndCapture(board, position, piece.getTeamColor());
+            List<ChessPosition>
+        }
         return Collections.emptyList();
     }
 }
