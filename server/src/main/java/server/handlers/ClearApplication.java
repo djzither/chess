06977b327eaa53d2f10b6659f820705ad37b2f38
Derek <dj.zither@gmail.com>
2026-02-24 17:27:29 -0700

    starting to work on clear

--- server/src/main/java/server/handlers/ClearApplication.java
+++ server/src/main/java/server/handlers/ClearApplication.java
@@ -1,4 +1,25 @@
 package server.handlers;
 
-public class ClearApplication {
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import server.service.ClearService;
+import server.service.UserService;
+import server.service.requestobjects.RegisterRequest;
+import server.service.requestobjects.RegisterResult;
+
+import java.util.Map;
+
+public class ClearApplication implements Handler {
+    private final ClearService clearService;
+
+
+    public ClearApplication(ClearService clearService) {
+        this.clearService = clearService;
+    }
+    public void handle(Context context){
+
+    }
+
+
 }
