dfc163980692e430c11b1fd2e4370cba1f9bed7e
Derek <dj.zither@gmail.com>
2026-03-05 15:01:46 -0700

    got it to work so that I could have the database initialized by this class

---
+++ server/src/main/java/dataaccess/DataBaseInitialize.java
@@ -0,0 +1,54 @@
+package dataaccess;
+
+import dataaccess.exceptions.DataAccessException;
+
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DataBaseInitialize {
+
+    public static void initialize() throws DataAccessException {
+        try {
+            DatabaseManager.createDatabase();
+
+            try (Connection conn = DatabaseManager.getConnection();
+                 Statement chess = conn.createStatement()) {
+
+                String users = """
+                        CREATE TABLE IF NOT EXISTS users (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        username varchar(256) NOT NULL,
+                        password varchar(256) NOT NULL,
+                        PRIMARY KEY ('id'),
+                        )
+                        """;
+                chess.executeUpdate(users);
+
+                String games = """
+                        CREATE TABLE IF NOT EXISTS games (
+                        game_id INT PRIMARY KEY,
+                        data TEXT NOT NULL
+                        )
+                        """;
+
+                chess.executeUpdate(games);
+
+
+                String auth = """
+                        CREATE TABLE IF NOT EXISTS auth (
+                        auth_token VARCHAR(256) PRIMARY KEY,
+                        username VARCHAR(256)
+                        )""";
+                chess.executeUpdate(auth);
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException("Didn't initialize database");
+        }
+
+
+    }
+}
