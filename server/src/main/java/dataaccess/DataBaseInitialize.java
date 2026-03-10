e19b32ebd65d555680eaeef710d3f4c643cb008e
Derek <dj.zither@gmail.com>
2026-03-10 10:36:43 -0600

    made it work with objects and better error handling

--- server/src/main/java/dataaccess/DataBaseInitialize.java
+++ server/src/main/java/dataaccess/DataBaseInitialize.java
@@ -31,8 +31,11 @@ public class DataBaseInitialize {
 
                 String games = """
                         CREATE TABLE IF NOT EXISTS games (
-                        game_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        data TEXT NOT NULL
+                        gameId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        whiteUsername VARCHAR(256) NOT NULL,
+                        blackUsername VARCHAR(256) NOT NULL,
+                        gameName VARCHAR(256) NOT NULL,
+                        game TEXT NOT NULL
                         )
                         """;
 
@@ -42,12 +45,12 @@ public class DataBaseInitialize {
                 String auth = """
                         CREATE TABLE IF NOT EXISTS auth (
                         auth_token VARCHAR(256) PRIMARY KEY,
-                        username VARCHAR(256)
+                        username VARCHAR(256) NOT NULL
                         )""";
                 chess.executeUpdate(auth);
             }
         } catch (DataAccessException | SQLException e) {
-            throw new DataAccessException("Didn't initialize database");
+            throw new DataAccessException("Didn't initialize database" + e.getMessage(), e);
         }
 
 
