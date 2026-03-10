6fb62327c74956c219a1b359241278a89b069074
Derek <dj.zither@gmail.com>
2026-03-10 16:39:08 -0600

    made the generate id stuff in CreateGame

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -84,11 +84,15 @@ public class MySqlDataAccess implements DataAccess {
     }
 
     @Override
-    public void createGame(GameData game) throws DataAccessException {
+    public int createGame(GameData game) throws DataAccessException {
         var statement = "INSERT INTO games (whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?)";
         String json = new Gson().toJson(game.game());
-        executeUpdate(statement, game.whiteUsername(), game.blackUsername(),
+        int genId = executeUpdate(statement, game.whiteUsername(), game.blackUsername(),
                         game.gameName(), json);
+        return genId;
+
+
+
         //i think that my generate id is in here but shoudl be later
     }
 
