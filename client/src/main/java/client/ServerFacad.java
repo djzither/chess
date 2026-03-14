eeada5635e344c9585f1e5f36077ccfa2ac22830
Derek <dj.zither@gmail.com>
2026-03-14 11:20:42 -0600

    these are all the new methods for post login

--- client/src/main/java/client/ServerFacad.java
+++ client/src/main/java/client/ServerFacad.java
@@ -1,9 +1,20 @@
 package client;
 
+import model.GameData;
+
+import java.util.List;
+
 public class ServerFacad {
     public static void login(String userName, String password) {
     }
 
     public static void register(String userName, String password, String email) {
+    }
+
+    public static void createGame(String gameName) {
+    }
+
+    public static List<GameData> listGames() {
+        return null;
     }
 }
