fa0f80dcf4da42559cf8acb6072075541bcbfd9f
Derek <dj.zither@gmail.com>
2026-03-18 16:47:42 -0600

    tbh, I just finished board so it looks good, gave up on the fancy characters

--- client/src/main/java/ui/MakeBoard.java
+++ client/src/main/java/ui/MakeBoard.java
@@ -36,142 +36,96 @@ public class MakeBoard {
     private static void drawColHeaders(PrintStream out, boolean isBlack){
         setBlack(out);
 
-        out.print(EMPTY);
-        String[] topHeaders = {"A","B","C", "D","E", "F", "G", "H"};
-
+        out.print("   ");
+        String[] topHeaders = {" A ", " B ", " C ", " D ", " E ", " F ", " G ", " H "};
         if (!isBlack) {
             for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++) {
-                drawTopHeaders(out, topHeaders[i] + " ");
+                drawTopHeaders(out, topHeaders[i]);
             }
         }
         else{
             for (int i = BOARD_SIZE_IN_SQUARES - 1; i >= 0; i--){
-                drawTopHeaders(out, topHeaders[i] + " ");
+                drawTopHeaders(out, topHeaders[i]);
             }
         }
         out.println();
+        out.print(RESET_BG_COLOR);
+        out.print(RESET_TEXT_COLOR);
     }
 
 
     private static void drawTopHeaders(PrintStream out, String topHeader){
         setBlack(out);
-        out.print(" " + topHeader + " ");
+        out.print("" + topHeader + "");
+        out.print(RESET_BG_COLOR);
+        out.print(RESET_TEXT_COLOR);
     }
 
     public enum pieceFirst {
-        /**
-         *
-         */
+
         BLACK_FIRST,
         WHITE_FIRST
     }
 
-
     private static void drawRowOfSquares(PrintStream out, pieceFirst which_color_first, int boardRow, boolean isBlack) {
-        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; squareRow++) {
+        int displayRow = !isBlack ? BOARD_SIZE_IN_SQUARES - boardRow : boardRow + 1;
 
-            int displayRow;
+        setBlack(out);
+        out.print(" " + displayRow + " ");
 
-            if (!isBlack) {
-                displayRow = BOARD_SIZE_IN_SQUARES - boardRow;
+        for (int colBoard = 0; colBoard < BOARD_SIZE_IN_SQUARES; colBoard++) {
+            int actualRow = !isBlack ? 8 - boardRow : boardRow + 1;
+            int actualCol = !isBlack ? colBoard + 1 : 8 - colBoard;
+
+            if ((actualRow + actualCol) % 2 == 0) {
+                out.print(SET_BG_COLOR_LIGHT_GREY);
             } else {
-                displayRow = boardRow + 1;
+                out.print(SET_BG_COLOR_BLUE);
             }
 
-
-            if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
-                out.print(displayRow + " ");
-
+            if (actualRow == 1 || actualRow == 2) {
+                out.print(SET_TEXT_COLOR_WHITE);
             } else {
-                out.print(EMPTY);
+                out.print(SET_TEXT_COLOR_BLACK);
             }
 
-            for (int colBoard = 0; colBoard < BOARD_SIZE_IN_SQUARES; colBoard++) {
-                int actualRow;
-                int actualCol;
-
-
-                if (!isBlack) {
-                    actualRow = 8 - boardRow;
-                    actualCol = colBoard + 1;
-                } else {
-                    actualRow = boardRow + 1;
-                    actualCol = 8 - colBoard;
-                }
-
-                if ((actualRow + actualCol) % 2 == 0) {
-                    out.print(SET_BG_COLOR_WHITE);
-                } else {
-                    setBlack(out);
-                }
-
-                if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
-                    int beg = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-                    int end = SQUARE_SIZE_IN_PADDED_CHARS - beg - 1;
-
-                    out.print(" ".repeat(beg));
-                    String piece = getPiece(actualRow, actualCol);
-                    out.print(piece);
-                    out.print(" ".repeat(end));
-                } else {
-                    out.print(EMPTY);
-                }
-
-            }
-
-
+            out.print(" " + getPiece(actualRow, actualCol) + " ");
         }
 
-
+        out.print(RESET_BG_COLOR);
+        out.print(RESET_TEXT_COLOR);
+        out.println();
     }
 
     private static String getPiece(int row, int col){
-        if (row == 8) {
+        if (row == 8 || row == 1) {
             if (col == 1 || col == 8) {
-                return BLACK_ROOK;
+                return "R";
             }
             if (col == 2 || col == 7) {
-                return BLACK_KNIGHT;
+                return "N";
             }
             if (col == 3 || col == 6) {
-                return BLACK_BISHOP;
+                return "B";
             }
             if (col == 4) {
-                return BLACK_QUEEN;
+                return "Q";
             }
             if (col == 5) {
-                return BLACK_KING;
+                return "K";
             }
         }
-        if (row == 7){
-            return BLACK_PAWN;
+        if (row == 7 || row == 2){
+            return "P";
         }
+        return " ";
 
-        if (row == 1) {
-            if (col == 1 || col == 8) {
-                return WHITE_ROOK;
-            }
-            if (col == 2 || col == 7) {
-                return WHITE_KNIGHT;
-            }
-            if (col == 3 || col == 6) {
-                return WHITE_BISHOP;
-            }
-            if (col == 4) {
-                return WHITE_QUEEN;
-            }
-            if (col == 5) {
-                return WHITE_KING;
-            }
-        }
-        if (row == 2){
-            return WHITE_PAWN;
-        }
-        return EMPTY;
     }
 
     private static void setBlack(PrintStream out){
         out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
     }
+
+
 }
