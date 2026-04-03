781b5e115cf4880b78112c1bdfde682ce555b556
Derek <dj.zither@gmail.com>
2026-04-02 18:00:58 -0600

    trying to have all my cases for what commands can be sent

--- client/src/main/java/ui/WSClient.java
+++ client/src/main/java/ui/WSClient.java
@@ -1,100 +1,114 @@
 package ui;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
+import jakarta.websocket.*;
+import org.eclipse.jetty.websocket.api.WebSocketContainer;
+import websocket.commands.MoveCommand;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
 import exception.ResponseException;
 
+
+import javax.management.Notification;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static com.sun.org.apache.xpath.internal.XPathAPI.eval;
-
 
 public class WSClient {
-    private final Integer gameId;
+    private Session session;
     private final ChessGame game;
-    private boolean running = false;
+    private final Integer gameId;
+    private final String authToken;
     private ChessGame.TeamColor playerColor;
+    private final Gson gson = new Gson();
 
-    public WSClient(Integer gameId, ChessGame currentGame) {
-        this.gameId = gameId;
-        this.game = currentGame;
+
+    public WSClient(String serverUrl, Integer gameId, ChessGame currentGame, String authToken) {
+        try {
+
+
+            this.gameId = gameId;
+            this.game = currentGame;
+            this.authToken = authToken;
+            serverUrl = serverUrl.replace("http", "ws") + "/ws";
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, new URI(serverUrl));
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = gson.fromJson(message, ServerMessage.class);
+                    handleServerMessage(serverMessage);
+
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+
+
+    }
+    @Override
+    public void onOpen(Session session, EndpointConfig config){
 
     }
 
-    public void connect(ChessGame.TeamColor color) {
-        this.playerColor = color;
-        this.running = true;
-        runGameLoop();
-    }
-
-    private void runGameLoop(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Joined game" + gameId + "\n Type 'help' for commands.");
-        var result = "";
-        while (running){
-            printPrompt();
-            String input = scanner.nextLine().trim();
-            try {
-                result = eval(input);
-                System.out.print(result);
-            }catch(Throwable e){
-                var msg = e.toString();
-                System.out.print(msg);
+    private void handleServerMessage(ServerMessage msg){
+        switch (msg.getServerMessageType()){
+            case LOAD_GAME -> {
+                MakeBoard board = new MakeBoard(game);
+                board.makeBoard(playerColor, null);
             }
+            case NOTIFICATION -> {
+                System.out.println("Notification! " + msg);
+            }
+            case ERROR -> {
+                System.err.println("Error! "+ msg);
 
-
-
+            }
         }
     }
-    public String eval(String input){
+
+    public void connect(ChessGame.TeamColor color) throws ResponseException{
+        this.playerColor = color;
+        sendCommand(new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameId))
+
+    }
+
+    public void connectToGame(ChessGame.TeamColor color) throws ResponseException{
+        this.playerColor = color;
+        sendCommand(new UserGameCommand((UserGameCommand.CommandType.CONNECT, authToken, gameId)))
+    }
+
+    public void sendMove(String from, String to) throws ResponseException{
+        MoveCommand move = new MoveCommand(authToken, gameId, from, to);
+        sendCommand(move);
+    }
+
+    public void resign() throws ResponseException{
+        sendCommand(new UserGameCommand((UserGameCommand.CommandType.RESIGN, authToken, gameId)));
+
+    }
+
+    public void leavegame() throws ResponseException{
+        sendCommand(new UserGameCommand((UserGameCommand.CommandType.LEAVE, authToken, gameId));
         try{
-            String[] tokens = input.split(" ");
-            String cmd = tokens[0].toLowerCase();
-            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
-
-            return switch (cmd) {
-                case "redraw" -> redraw(params);
-                case "leave" -> leave(params);
-                case "move" -> move(params);
-                case "resign" -> resign(params);
-                case "legalMoves" -> legalMoves(params);
-                default -> help();
-
-            };
-
-        }catch (ResponseException ex){
-
-            return ex.getMessage();
+            session.close();
+        } catch (IOException ex){
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
         }
-
-    }
-    private String help() {
-        return """
-                    - redraw -> will redraw chessboard
-                    - leave -> will leave game
-                    - move -> <A5> <B4>
-                    - resign -> will forefit game
-                    - legalMoves <B4> -> will highlight legal moves for selected piece
-                    """;
     }
 
-    private String legalMoves(String[] params) {
+    private void sendCommand(Object cmd) throws ResponseException{
+        try{
+            String json = gson.toJson(cmd);
+            session.getBasicRemote().sendText(json);
+        } catch (IOException e){
+            throw new ResponseException(ResponseException.Code.ServerError, e.getMessage());
+        }
     }
 
-    private String resign(String[] params) {
-    }
-
-    private String move(String[] params) {
-    }
-
-    private String leave(String[] params) {
-    }
-
-
-
-    private void redraw(String[] params) {
-        MakeBoard board = new MakeBoard(game);
-        board.makeBoard(playerColor, null);
-    }
-
-}
