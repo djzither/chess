41f06c72c34dc134700f227a147bda7ce25f62d4
Derek <dj.zither@gmail.com>
2026-04-04 13:32:24 -0600

    imports work

--- client/src/main/java/ui/WSClient.java
+++ client/src/main/java/ui/WSClient.java
@@ -7,6 +7,9 @@ import jakarta.websocket.Session;
 
 import websocket.commands.MoveCommand;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessages;
+import websocket.messages.LoadGame;
+import websocket.messages.Notification;
 import websocket.messages.ServerMessage;
 import exception.ResponseException;
 
@@ -60,14 +63,18 @@ public class WSClient extends Endpoint{
     private void handleServerMessage(ServerMessage msg){
         switch (msg.getServerMessageType()){
             case LOAD_GAME -> {
+                //type casts are cool
+                LoadGame loadMsg = (LoadGame) msg;
                 MakeBoard board = new MakeBoard(game);
-                board.makeBoard(playerColor, null);
+                board.makeBoard(playerColor, loadMsg.getBoardinJson());
             }
             case NOTIFICATION -> {
-                System.out.println("Notification! " + msg);
+                Notification notification = (Notification) msg;
+                System.out.println("Notification! " + notification.getMessage());
             }
             case ERROR -> {
-                System.err.println("Error! "+ msg);
+                ErrorMessages errorMessages = (ErrorMessages) msg;
+                System.err.println("Error! "+ errorMessages.getErrorText());
 
             }
         }
