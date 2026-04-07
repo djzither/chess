ba60930474f1da5a2628882ade1657df08484e1a
Derek <dj.zither@gmail.com>
2026-04-07 15:37:23 -0600

    added a method so that I coudl do chess position for the move stuff

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,4 +7,22 @@ public record ChessPosition(int row, int col){
     public int getColumn(){
         return col;
     }
+
+    public static ChessPosition fromString(String pos){
+        if (pos == null || pos.length() != 2){
+            throw new IllegalArgumentException("Invalid chess move");
+
+        }
+
+        char oneChar = pos.charAt(0);
+        char twoNum = pos.charAt(1);
+
+        int col = oneChar - 'a' + 1;
+        int row = Character.getNumericValue(twoNum);
+        if (col < 1 || col > 8 || row < 1 || row > 8){
+            throw new IllegalArgumentException("bad input");
+
+        }
+        return new ChessPosition(row, col);
+    }
 }
