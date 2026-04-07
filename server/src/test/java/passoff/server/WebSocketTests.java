a5145a8497461c2855b429b88ace7abece47c2f8
Derek <dj.zither@gmail.com>
2026-04-07 17:22:22 -0600

    auth token

--- server/src/test/java/passoff/server/WebSocketTests.java
+++ server/src/test/java/passoff/server/WebSocketTests.java
@@ -14,7 +14,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static websocket.messages.ServerMessage.ServerMessageType.*;
-
+//
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class WebSocketTests {
     private static WebsocketTestingEnvironment environment;
